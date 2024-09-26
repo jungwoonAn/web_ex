b065f53524e193a265a7d654b583b964d4e41ec7
jungwoonAn <red0808@naver.com>
2024-09-26 17:54:23 +0900

    20240926

--- MVC07/src/main/java/kr/bit/controller/FileGetController.java
+++ MVC07/src/main/java/kr/bit/controller/FileGetController.java
@@ -1,6 +1,10 @@
 package kr.bit.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URLEncoder;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +19,35 @@ public class FileGetController implements Controller {
 		String filename = request.getParameter("filename");
 		System.out.println(filename);
 		
+		String UPLOAD_DIR = "file_repo";
+		String uploadPath = request.getServletContext().getRealPath("") + File.separator + UPLOAD_DIR;
+		File f = new File(uploadPath + "\\" + filename);
+		
+		// 클라이언트로부터 넘어오는 파일이름에 한글이 있는 경우 깨지지 않게 하기 위함
+		filename = URLEncoder.encode(filename, "utf-8");
+		filename = filename.replace("+", " ");  // 크롬브라우저의 경우
+		// 다운로드 준비 서버에서 클라이언트에게 다운로드 준비를 시키는 부분(다운로드 창을 띄운다)
+		response.setContentLength((int)f.length());
+		response.setContentType("application/x-msdownload;charset=utf-8");
+		response.setHeader("Content-Disposition", "attachment;filename="+filename+";");
+		response.setHeader("Content-Transfor-Encoding", "binary");
+		response.setHeader("Pragma", "no-cache");
+		response.setHeader("Expires", "0");
+		
+		// 실제 다운로드를 하는 부분
+		FileInputStream in = new FileInputStream(f); // 파일 읽기 준비
+		OutputStream out = response.getOutputStream();
+		byte[] buffer = new byte[1024];
+		
+		while(true) {
+			int count = in.read(buffer);
+			if(count == -1) {
+				break;
+			}
+			out.write(buffer, 0, count);  // 다운로드(0%.....100%)
+		}
+		in.close();
+		out.close();	
 		
 		return null;
 	}
